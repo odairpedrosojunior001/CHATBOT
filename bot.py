d68339c53601d69fb3891ddb63950a05a2c33bc9
Odair Pedroso Junior - TI <oai_junior@magazineluiza>
2019-03-12 13:19:50 -0300

    bot

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 #-*-coding: utf-8 -*-
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer #importar 2 bibliotecas ( lista treinamento ; bot)
 from chatterbot import chatterbot
 
-bot = ChatBot('test')
+bot=ChatBot('test') 
 
-convImei = ["oi","olá","olá","bom dia","como faturar um pedido de celular no mobile?","realizar a separação no mobile,coletar IMEI e finalizar"]
+conv_Imei=["oi","olá","olá","bom dia","como faturar um pedido de celular no mobile?","realizar a separação no mobile,coletar IMEI e finalizar","muito obrigado","de nada"]
+
+bot.set_trainer(ListTrainer) #iniciar a lista com bot
+bot.train(conv_IMEI) #treinar a lista com Machine Learning
+
+while True: # laço para a interação de perguntas e respostas do bot 
+    pergunta = input("Você:")
+    resposta = bot.get_rsposta(pergunta)
+
+print("Bot:",resposta)
+
 
