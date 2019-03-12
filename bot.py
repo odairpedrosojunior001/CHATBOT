0737eff9c25d4e761020ef37ba8973c087e687fb
Odair Pedroso Junior - TI <oai_junior@magazineluiza>
2019-03-12 11:12:21 -0300

    bot

---
+++ bot.py
@@ -0,0 +1,8 @@
+#-*-coding: utf-8 -*-
+from chatterbot.trainers import ListTrainer
+from chatterbot import chatterbot
+
+bot = ChatBot('test')
+
+convImei = ["oi","olá","olá","bom dia","como faturar um pedido?","realizar a separação no mobile,coletar IMEI e finalizar"]
+
