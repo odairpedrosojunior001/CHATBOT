efaf1bf3dc2f2863d623167a7824bca71e589329
Odair Pedroso <odair.junior@luizalabs.com>
2019-03-13 00:25:05 -0300

    bot

--- bot.py
+++ bot.py
@@ -17,10 +17,9 @@ convImei=[
 bot.set_trainer(ListTrainer) #iniciar a lista com bot
 bot.train(convImei) #treinar a lista com Machine Learning
 
-while True: # laço para a interação de perguntas e respostas do bot 
-    quest = input("Você:")
+while True:  # laço para a interação de perguntas e respostas do bot
+    quest = input("Você: ")
     response = bot.get_response(quest)
-
-print("Bot:",response) # print com o retorno da resposta do bot se estiver dentro do laço e da variavel de lista.
+    print("Bot: ", response)
 
 
